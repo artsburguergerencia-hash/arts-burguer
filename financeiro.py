8c617713b100f35585a40588b5d8dd2c89dfc485
artsburguergerencia-hash <artsburguergerencia@gmail.com>
2026-08-14 01:15:34 -0300

    Update financeiro.py

--- financeiro.py
+++ financeiro.py
@@ -24,12 +24,13 @@ class StatusCaixa(str, enum.Enum):
 # ==============================================================================
 
 class FornecedorModel(Base):
+    """Cadastro de Fornecedores de insumos do Art's Burguer."""
     __tablename__ = "fornecedores"
     __table_args__ = {'extend_existing': True}
 
     id = Column(Integer, primary_key=True, index=True)
     nome_fantasia = Column(String, nullable=False)
-    cnpj = Column(String, nullable=True) # Retirado o unique=True que estava quebrando
+    cnpj = Column(String, nullable=True) # <- Retirado o unique=True para não travar
     telefone = Column(String, nullable=True, default="")
     contato = Column(String, nullable=True, default="")
     categoria = Column(String, default="Geral") 
